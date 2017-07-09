9e159e2d83843cbd0624f5736e367674a9ce944e
Guilherme Caraciolo <gcaraciolo@gmail.com>
2017-07-09 12:06:42 -0300

    Remove default values of DB from constants.

--- constants.js
+++ constants.js
@@ -9,10 +9,10 @@ const parseBoolean = (v) => v == 'true'
 //Database
 module.exports.DB_USER = process.env.DB_USER
 module.exports.DB_PASSWORD = process.env.DB_PASSWORD
-module.exports.DB_NAME = process.env.DB_NAME || 'pokemons'
+module.exports.DB_NAME = process.env.DB_NAME
 module.exports.DB_HOST = process.env.DB_HOST
-module.exports.DB_PORT= process.env.DB_PORT
-module.exports.DB_DIALECT= process.env.DB_DIALECT || 'sqlite'
+module.exports.DB_PORT = process.env.DB_PORT
+module.exports.DB_DIALECT = process.env.DB_DIALECT
 
 //Pagarme
 module.exports.PAGARME_API_KEY = process.env.PAGARME_API_KEY
