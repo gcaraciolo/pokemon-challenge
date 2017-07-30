f2f2b44da4dd5e5c0e8acd21a00e7508a16ff087
Guilherme Caraciolo <gcaraciolo@gmail.com>
2017-07-30 12:57:42 -0300

    change variable name: app to pokemonRoutes

--- src/pokemon/pokemonRoutes.js
+++ src/pokemon/pokemonRoutes.js
@@ -1,25 +1,26 @@
-const app = require('express').Router()
+const pokemonRoutes = require('express').Router()
 const Joi = require('joi')
 const paramValidation = require('express-validation')
 
 const pokemonController = require('./pokemonController')
 
-app.get('/pokemons', pokemonController.list)
+pokemonRoutes.get('/pokemons', pokemonController.list)
 
-app.post('/pokemons',
+pokemonRoutes.post('/pokemons',
   paramValidation({
     body: {
       name: Joi.string().alphanum()
         .min(1).max(255)
         .required(),
-      price: Joi.number().required(),
+      price: Joi.number()
+        .required().label('Pokemon price'),
       stock: Joi.number().integer()
     }
   }),
   pokemonController.create
 )
 
-app.post('/pokemons/buy',
+pokemonRoutes.post('/pokemons/buy',
   paramValidation({
     body: {
       name: Joi.string().alphanum()
@@ -31,4 +32,4 @@ app.post('/pokemons/buy',
   pokemonController.buy
 )
 
-module.exports = app
+module.exports = pokemonRoutes
