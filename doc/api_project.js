338987d0847044bd2c4425185cf4bf7d1cb30f58
Guilherme Caraciolo <gcaraciolo@gmail.com>
2017-08-07 15:17:11 -0300

    Versioning api doc files

---
+++ doc/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "Pokemon Challenge",  "version": "1.0.1",  "description": "Pokemon challenge api example",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2017-08-07T18:16:21.408Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
